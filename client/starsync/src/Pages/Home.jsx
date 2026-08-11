86711435b8805af007e431aa5e55ed6da70543e9
daniel <danielkrg11@gmail.com>
2026-08-10 22:48:35 -0400

    forgot the import

--- client/starsync/src/Pages/Home.jsx
+++ client/starsync/src/Pages/Home.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import { useNavigate } from "react-router-dom";
+import { useUserData } from '../Components/UserDataContext';
 // import Waves from '../assets/SVGs/layeredWavesHome.svg?react'
 
 function Home({ preview = false }) {
