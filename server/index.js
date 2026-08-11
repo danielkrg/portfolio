afe8e810bbce569b181448f96957933ede1e3edf
daniel <danielkrg11@gmail.com>
2026-08-10 23:28:48 -0400

    cookies

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ app.use(session({
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
-    cookie: { secure: false },
+    cookie: { secure: true, sameSite: 'none', httpOnly: true, },
 }));
 
 // Mount routers
